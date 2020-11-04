e92d0df96e651eab57a5e799dfa6ced26de1a7a8
Alan Borning <borning@cs.washington.edu>
2020-11-03 17:09:15 -0800

    also delete bus line info for virtual meetings

--- bread-virtual-meetings.php
+++ bread-virtual-meetings.php
@@ -12,7 +12,8 @@ add_filter( 'Bread_Enrich_Meeting_Data', 'fixVirtualMeetings', 10, 2 );
 /*
 The default presentation in bread for virtual meetings that are replacing an in-person meeting seems potentially
 misleading, since it may be easy to overlook the TC key and go to a closed meeting location. This extension fixes up
-those listings by adding a note in front of the facility name, and removing the address.
+those listings by adding a note in front of the facility name, and removing the address. It also removes the
+bus information to save space on the printed schedules, since this isn't needed to get to a virtual meeting.
 */
 function fixVirtualMeetings($value, $formats_by_key) {
     if (in_array('TC', explode( ",", $value['formats']))) {
@@ -23,6 +24,7 @@ function fixVirtualMeetings($value, $formats_by_key) {
         $value['location_street'] = '';
         // could also delete the zip code ....
         // $value['location_postal_code_1'] = '';
+        $value['bus_lines'] = '';
     }
     return $value;
 }

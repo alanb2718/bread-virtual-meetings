62d42ab8b28e1a1bd93e8c274beb97d7b6905a14
Alan B <918773+alanb2718@users.noreply.github.com>
2021-11-12 10:50:58 -0800

    update README, comments; bump version number

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 
 The default presentation in [bread](https://github.com/bmlt-enabled/bread) for virtual meetings that are replacing an
 in-person meeting seems potentially misleading, since it may be easy to overlook the TC key and go to a closed meeting
-location. This extension fixes up those listings by adding a note in front of the facility name, and removing the address.
+location. This extension provides some options for fixing those listings.  There are three options:
+1. Add a note in front of the facility name: `Currently online only -- normally at`
+2. Remove the facility name altogether, as well as the TC format
+3. Don't change anything
+
+Option 1 is closer to what crouton does.  Option 2 saves space on the printed schedule.
+For options 1 and 2 this extension also removes any additional location information, the street address, and bus information,
+since none of this is needed to get to a virtual meeting.
+
+Option 3 is mostly useful for multisite environments -- otherwise it would be weird to be using this plugin at all.
+
+There is a top-level administration menu item to select the option.  Making it a top-level menu is weird, but seems to be
+the best of various bad choices.  Putting it under Meeting List (the bread menu) would be plausible, except that this option
+isn't saved as part of the bread configuration, making it easy to overlook.  (Having bread save it would require some
+additional hooks in bread itself -- easier would be to just add this functionality to bread.)  Putting it under "Settings"
+would normally be good, except that bread uses a top-level menu item, and it seems like this one would get lost there.
--- bread-virtual-meetings.php
+++ bread-virtual-meetings.php
@@ -4,32 +4,21 @@ Plugin Name: bread-virtual-meetings
 Plugin URI: none
 Description: Reformat virtual meeting listings in bread
 Author: Alan B
-Version: 1.0
+Version: 1.1
 */
 
 add_filter( 'Bread_Enrich_Meeting_Data', 'fixVirtualMeetings', 10, 2 );
 
 /*
 The default presentation in bread for virtual meetings that are replacing an in-person meeting seems potentially
-misleading, since it may be easy to overlook the TC key and go to a closed meeting location. This extension fixes up
-those listings.  There are three options:
-    1) adding a note in front of the facility name: 'Currently online only -- normally at '
-    2) removing the facility name altogether, as well as the TC format
-    3) don't change anything (maybe useful for a multisite environment -- otherwise it would be weird to be using this plugin at all)
-Option 1 is closer to what crouton does.  Option 2 saves space on the printed schedule.
-For options 1 and 2 this extension also removes any additional location information, the street address, and bus information,
-since none of this is needed to get to a virtual meeting.
-
-There is a top-level administration menu item to select the option.  Making it a top-level menu is weird, but seems to be
-the best of various bad choices.  Putting it under Meeting List (the bread menu) would be plausible, but this is special;
-it isn't saved as part of the bread configuration.  (Having bread save it would require some additional hooks in bread itself --
-easier would be to just add this functionality to bread.)  Putting it under "Settings" would normally be good, except that
-bread uses a top-level menu item, and it seems like this one would get lost there.
+misleading, since it may be easy to overlook the TC key and go to a closed meeting location. This extension provides
+ways to fix up those listings.
 */
 
 function fixVirtualMeetings($value, $formats_by_key) {
-    $option = get_option('breadxtn_options')['breadxtn_field_option'];
-    // $option should be one of 'no-changes', 'omit-location', or 'add-online-only'
+    $options = get_option('breadxtn_options', ['breadxtn_field_option' => 'add-online-only']);
+    // $option should be one of 'no-changes', 'omit-location', or 'add-online-only'.  The default is 'add-online-only'.
+    $option = $options['breadxtn_field_option'];
     if ($option === 'omit-location' || $option === 'add-online-only') {
         $format_array = explode(',', $value['formats']);
         $i = array_search('TC', $format_array);   // $i will be false if no TC format, otherwise its index
